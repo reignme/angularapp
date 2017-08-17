22f2682bf55b995cc14bc20901bb7e1048c5954d
Woosuk Chang <changw@prd00701.lan>
2017-08-16 23:12:17 -0700

    chap 18

---
+++ src/app/components/sandbox/sandbox.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-sandbox',
+    templateUrl: './sanbox.component.html',
+    styleUrls: ['./sanbox.component.css']
+    // styles: [
+    // `
+    //     .special{
+    //         color:green;
+    //         font-size:20px;
+    //         text-transform:uppercase
+    //     }
+    // `
+    // ]
+})
+
+export class SandboxComponent {
+    constructor() {
+        
+    }
+}
